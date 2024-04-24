7da6962a8cee9023dc3c9ce1d34fd0ee0afd3eb1
alireza_sariri <alirezasariri78@gmail.com>
2024-04-24 15:17:39 +0330

    implement sorting for table

--- src/diagram/table.rs
+++ src/diagram/table.rs
@@ -1,12 +1,16 @@
-use crate::args::CommandArgs;
 use crate::crawler::Node;
+use crate::{
+    args::{CommandArgs, SortType},
+    util::thousends_seperator,
+};
 use cli_table::{format::Justify, Cell, CellStruct, Style, Table, TableStruct};
 use rs_abbreviation_number::NumericAbbreviate;
 use std::{ops::Deref, sync::Arc};
 
 pub fn create_table_diagram(tree: &Arc<Node>, args: &CommandArgs) -> TableStruct {
-    let mut data: Vec<Vec<CellStruct>> = Vec::new();
-    crawl_tree(tree, args, &mut data);
+    let mut nodes: Vec<Arc<Node>> = Vec::new();
+    crawl_tree(tree, args, &mut nodes);
+    let data = export_to_table(nodes, args);
     data.table()
         .title(vec![
             "Name".cell().justify(Justify::Center).bold(true),
@@ -15,23 +19,58 @@ pub fn create_table_diagram(tree: &Arc<Node>, args: &CommandArgs) -> TableStruct
         .bold(true)
 }
 
-fn crawl_tree(tree: &Arc<Node>, args: &CommandArgs, result: &mut Vec<Vec<CellStruct>>) {
+fn export_to_table(nodes: Vec<Arc<Node>>, args: &CommandArgs) -> Vec<Vec<CellStruct>> {
+    let mut result: Vec<Vec<CellStruct>> = Vec::new();
+    let sorted_data = sort_data(&nodes, args.sort.clone());
+    for (name, size) in sorted_data {
+        let size = format!(
+            "{}iB ({} bytes)",
+            size.abbreviate_number(),
+            thousends_seperator(size)
+        );
+        result.push(vec![
+            name.cell().justify(Justify::Center),
+            size.cell().justify(Justify::Center),
+        ]);
+    }
+    result
+}
+
+fn crawl_tree(tree: &Arc<Node>, args: &CommandArgs, result: &mut Vec<Arc<Node>>) {
     if tree.get_depth().get() == args.depth && args.depth != 0 {
         return;
     }
     let childrens = tree.get_childes().borrow();
     let deref = childrens.deref();
     for child in deref {
-        let node = child.deref();
-        let size = format!(
-            "{}iB ({} bytes)",
-            node.get_size().get().abbreviate_number(),
-            node.get_size().get()
-        );
-        result.push(vec![
-            node.get_name().cell().justify(Justify::Center),
-            size.cell().justify(Justify::Center),
-        ]);
+        result.push(child.clone());
         crawl_tree(child, args, result);
+    }
+}
+
+fn sort_data(input: &Vec<Arc<Node>>, sort_type: SortType) -> Vec<(&str, u64)> {
+    let mut data: Vec<(&str, u64)> = input
+        .iter()
+        .map(|i| (i.get_name().as_str(), i.get_size().get()))
+        .collect();
+
+    match sort_type {
+        SortType::Name(t) => {
+            if t {
+                data.sort_by(|a, b| a.0.cmp(b.0));
+            } else {
+                data.sort_by(|a, b| b.0.cmp(a.0));
+            }
+            data
+        }
+        SortType::Size(t) => {
+            if t {
+                data.sort_by(|a, b| a.1.cmp(&b.1));
+            } else {
+                data.sort_by(|a, b| b.1.cmp(&a.1));
+            }
+            data
+        }
+        _ => data,
     }
 }
