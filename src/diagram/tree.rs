496f2d63c8c46d89b957ebb6f9148c4ebb45b089
alireza_sariri <alirezasariri78@gmail.com>
2024-04-29 10:04:03 +0330

    in tree diagram we set color for branches based on their size

--- src/diagram/tree.rs
+++ src/diagram/tree.rs
@@ -1,5 +1,5 @@
 use rs_abbreviation_number::NumericAbbreviate;
-
+use colored::Colorize;
 use crate::args::CommandArgs;
 use crate::crawler::Node;
 use crate::util::*;
@@ -31,11 +31,7 @@ fn crawl_tree(tree: &Arc<Node>, args: &CommandArgs, result: &mut String) {
 }
 
 fn add_branch(node: &Node) -> String {
-    let mut branch_char = MIDDLE_CHAR;
-    if node.is_last_child() {
-        branch_char = END_CHAR;
-    }
-
+    let branch_char = get_branch_char(node);
     let mut size = node.get_size().get().abbreviate_number();
     size.push_str("iB");
     format!(
@@ -51,3 +47,21 @@ fn add_branch(node: &Node) -> String {
         )
     )
 }
+
+fn get_branch_char(node:&Node)->String{
+    const GREEN_SIZE_BYTES:u64=1_024_000_000;
+    const YELLOW_SIZE_BYTES:u64=GREEN_SIZE_BYTES*10;
+    const YELLOW_STAR:u64=GREEN_SIZE_BYTES+1; 
+ 
+    let mut branch_char = MIDDLE_CHAR;
+    if node.is_last_child() {
+        branch_char = END_CHAR;
+    }
+    match node.get_size().get() {
+        0..=GREEN_SIZE_BYTES=>branch_char.green().to_string(),
+        YELLOW_STAR..=YELLOW_SIZE_BYTES=>branch_char.yellow().to_string(),
+        _=>branch_char.red().to_string(),
+    }
+}
+
+
